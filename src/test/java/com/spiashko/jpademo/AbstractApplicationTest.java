c61f8f291d7a9cee96e2c6eb9f7a7bbb9d87b5b5
Siarhei Piashko <peshko.sergey@gmail.com>
2022-02-16 15:59:19 +0100

    refactoring

--- src/test/java/com/spiashko/jpademo/AbstractApplicationTest.java
+++ src/test/java/com/spiashko/jpademo/AbstractApplicationTest.java
@@ -2,12 +2,35 @@ package com.spiashko.jpademo;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 public abstract class AbstractApplicationTest {
+
+    public static JdbcDatabaseContainer<?> dbContainer = new PostgreSQLContainer<>("postgres:14")
+            .withDatabaseName("tests-db")
+            .withUsername("sa")
+            .withPassword("sa")
+            .withInitScript("postgre-init.sql")
+            .withEnv("TZ", "UTC")
+            .withEnv("PGTZ", "UTC")
+            .withReuse(true);
+
+    static {
+        dbContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void datasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", dbContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", dbContainer::getUsername);
+        registry.add("spring.datasource.password", dbContainer::getPassword);
+    }
 
 }
