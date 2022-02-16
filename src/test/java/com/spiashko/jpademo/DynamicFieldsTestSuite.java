98216a4fbe3721bff8b9d591bf48276b9a01dc6a
Siarhei Piashko <peshko.sergey@gmail.com>
2022-02-16 22:54:22 +0100

    improve

--- src/test/java/com/spiashko/jpademo/DynamicFieldsTestSuite.java
+++ src/test/java/com/spiashko/jpademo/DynamicFieldsTestSuite.java
@@ -4,6 +4,7 @@ import com.spiashko.jpademo.dynamicfields.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 
@@ -18,7 +19,7 @@ public class DynamicFieldsTestSuite extends AbstractApplicationTest {
     private static final String selectSql = """
             select *
             from person p
-                     left join person_field pf on p.id = pf.fk_person and pf.name = 'firstname'
+                     left join person_field pf on p.id = pf.fk_person and pf.name = ?
             order by pf.value
             """;
 
@@ -31,13 +32,23 @@ public class DynamicFieldsTestSuite extends AbstractApplicationTest {
     @Test
     public void complexTest() {
 
-        List<Map<String, Object>> peopleFromRawSql = jdbcTemplate.queryForList(selectSql);
+        List<Map<String, Object>> peopleFromRawSqlOrderedByFirstname = jdbcTemplate.queryForList(selectSql, "firstname");
+        Assertions.assertEquals(3, peopleFromRawSqlOrderedByFirstname.size());
 
-        Assertions.assertEquals(3, peopleFromRawSql.size());
+        List<Map<String, Object>> peopleFromRawSqlOrderedByKek = jdbcTemplate.queryForList(selectSql, "kek");
+        Assertions.assertEquals(3, peopleFromRawSqlOrderedByKek.size());
 
-        List<Person> people = repository.findAll((root, query, builder) -> {
+        List<Person> peopleFromCriteriaOrderedByFirstname = repository.findAll(prepareSpec("firstname"));
+        Assertions.assertEquals(3, peopleFromCriteriaOrderedByFirstname.size());
+
+        List<Person> peopleFromCriteriaOrderedByKek = repository.findAll(prepareSpec("kek"));
+        Assertions.assertEquals(3, peopleFromCriteriaOrderedByKek.size());
+    }
+
+    private Specification<Person> prepareSpec(String fieldName) {
+        return (root, query, builder) -> {
             ListJoin<Person, PersonField> join = root.join(Person_.personFields, JoinType.LEFT);
-            join.on(builder.equal(join.get(PersonField_.name), "firstname"));
+            join.on(builder.equal(join.get(PersonField_.name), fieldName));
             query
                     .where()
                     .orderBy(
@@ -45,16 +56,7 @@ public class DynamicFieldsTestSuite extends AbstractApplicationTest {
                     );
 
             return query.getRestriction();
-        });
+        };
 
-        Assertions.assertEquals(3, people.size());
     }
-
-    /*sql
-    // ;
-     */
-
-    /*
-    select *from participant left join participant_field pf on p.Id = pf.participant_id where p.Id In (2224532,234728,6272739)
-     */
 }
