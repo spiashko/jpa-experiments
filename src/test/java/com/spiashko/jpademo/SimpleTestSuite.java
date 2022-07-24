444afb8a067c069311d50b3bc6d6da6bafe13b89
Siarhei Piashko <siarhei.piashko@digitallyinspired.com>
2022-07-24 21:08:00 +0200

    add removeAndInsertTheSameEmployeeTest

--- src/test/java/com/spiashko/jpademo/SimpleTestSuite.java
+++ src/test/java/com/spiashko/jpademo/SimpleTestSuite.java
@@ -3,14 +3,23 @@ package com.spiashko.jpademo;
 import com.spiashko.jpademo.simple.Company;
 import com.spiashko.jpademo.simple.CompanyRepository;
 import com.spiashko.jpademo.simple.Employee;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import java.util.List;
+
+@Slf4j
 public class SimpleTestSuite extends AbstractApplicationTest {
 
     @Autowired
     private CompanyRepository companyRepository;
+
+    @Autowired
+    private TransactionTemplate template;
 
     @Test
     public void complexTest() {
@@ -45,4 +54,49 @@ public class SimpleTestSuite extends AbstractApplicationTest {
         companyRepository.deleteAll();
         Assertions.assertEquals(0, companyRepository.findAll().size());
     }
+
+    @Test
+    public void removeAndInsertTheSameEmployeeTest() {
+
+        Company google = Company.builder()
+                .name("Google")
+                .build();
+
+        Employee bob = Employee.builder()
+                .name("Bob")
+                .build();
+
+        Employee alice = Employee.builder()
+                .name("Alice")
+                .build();
+
+
+        google.addEmployee(bob);
+        google.addEmployee(alice);
+
+        companyRepository.save(google);
+
+        template.executeWithoutResult(s -> {
+            List<Company> companies = companyRepository.findAll();
+            Assertions.assertEquals(1, companies.size());
+            Assertions.assertEquals(2, companies.get(0).getEmployees().size());
+        });
+
+        Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+            template.executeWithoutResult(s -> {
+                Company googleInDb = companyRepository.findAll().get(0);
+
+                boolean aliceWasRemoved = googleInDb.getEmployees().removeIf(e -> e.getName().equals("Alice"));
+                Assertions.assertTrue(aliceWasRemoved, "alice should be removed from list");
+                //companyRepository.flush(); // flush will make it work again
+
+                Employee aliceOnceAgain = Employee.builder()
+                        .name("Alice")
+                        .build();
+                googleInDb.addEmployee(aliceOnceAgain);
+            });
+        });
+    }
+
+
 }
